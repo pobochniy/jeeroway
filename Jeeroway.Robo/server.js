8024e68e70fe3644c1b94c4dd0fab4a37ff93a62
pobochniy <phoenix_net@bk.ru>
2025-11-08 17:00:17 +0300

    robo

---
+++ Jeeroway.Robo/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const {spawn} = require('child_process');
+const app = express();
+const port = 3000;
+
+let ffmpegProcess = null;
+
+app.post('/start', (req, res) => {
+    if(ffmpegProcess){
+        return res.status(400).send('streaming already started');
+    }
+
+    const udpAddress = '192.168.1.33:5000';
+    const args = [
+        '-f', 'v4l2',
+        '-framerate', '15',
+        '-input_format', 'yuyv422',
+        '-video_size', '640x480',
+        '-i', '/dev/video0',
+        '-vcodec', 'mjpeg',
+        '-f', 'mjpeg',
+        'udp://'+udpAddress
+    ];
+
+    console.log('Launch ffmpeg with args:', args.join(' '));
+
+    ffmpegProcess = spawn('ffmpeg', args);
+
+    ffmpegProcess.stderr.on('data', data =>{
+        console.error(`ffmpeg: ${data}`);
+    });
+
+    ffmpegProcess.on('exit', (code)=>{
+        console.log(`ffmpeg exited with code ${code}`);
+        ffmpegProcess = null;
+    });
+    res.send('streaming started');
+});
+
+app.post('/stop', (req, res)=>{
+    if(!ffmpegProcess){
+        return res.status(400).send('No active stream');
+    }
+
+    ffmpegProcess.kill('SIGINT');
+    ffmpegProcess = null;
+    res.send('Streaming stopped');
+});
+
+app.listen(port, ()=>{
+    console.log(`Raspberry pi streaming control on port ${port}`);
+});
